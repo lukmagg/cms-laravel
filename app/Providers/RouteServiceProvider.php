984d171e5de7a085e3831eac07c7e11cea889ad6
lucas maggio <marsinformatica@gmail.com>
2020-05-21 15:34:00 +0200

    Creacion de archivo para rutas admin.php y creacion de middleware IsAdmin.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapAdminRoutes();
         //
     }
 
@@ -62,6 +63,15 @@ class RouteServiceProvider extends ServiceProvider
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
     }
+
+
+    #Esta funcion la agregamos nosotros
+    protected function mapAdminRoutes(){
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/admin.php'));
+    }
+
 
     /**
      * Define the "api" routes for the application.
---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+    Route::get('/admin', function(){
+        return 'holaa';
+    })
+
+
+
+?>
